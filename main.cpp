31bb91b23be0c86b29f70e9907f30171869d4a68
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2020-10-24 22:32:36 +0330

    remove main body

--- main.cpp
+++ main.cpp
@@ -4,16 +4,6 @@
 #include "User.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    Response *response = new Success(1, "message");
-
-    bool isSuccess = dynamic_cast<Response *>(response);
-    std::cout << isSuccess << std::endl;
-
-    if (instanceof<Success>(response)) {
-        std::cout << "haha response is instance of Success" << std::endl;
-    }
 
     return 0;
 }
